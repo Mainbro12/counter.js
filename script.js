b8571b6e8aef6990f6a625efc4a969bcb1a5dd1e
pinokio221 <pinokio221b@gmail.com>
2025-07-15 22:00:27 +0300

    Removed double click function

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
 
 let count = 0;
 
-function minus ()  {
-    count = count-1;
+function minus() {
+    count = count - 1;
     if (count < 0) {
-    count = 0;
+        count = 0;
     }
-    countdiv.textContent= count;
+    countdiv.textContent = count;
 }
-function plus () {
-    count = count+1;
+function plus() {
+    count = count + 1;
     if (count > 100)
         count = 100;
-countdiv.textContent= count;
+    countdiv.textContent = count;
 }
-function plusTen () {
-    count = count+10;
-    if (count > 100)
-        count = 100;
-countdiv.textContent= count;
-}
-function reset () {
+
+function reset() {
     count = 0;
-    countdiv.textContent= count;
+    countdiv.textContent = count;
 
 }
 
@@ -31,9 +26,8 @@ const countdiv = document.getElementById("count")
 const minusButton = document.getElementById("minus-button")
 const resetButton = document.getElementById("reset-button")
 
-countdiv.textContent= count;
+countdiv.textContent = count;
 
-addButton.addEventListener('dblclick', plusTen)
 addButton.addEventListener('click', plus)
 minusButton.addEventListener('click', minus)
 resetButton.addEventListener('click', reset)
